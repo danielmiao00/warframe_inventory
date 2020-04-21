8247019a0329ca986bd9dc46add8909ff73a5d24
Jessica Chen <jchen4181@gmail.com>
2020-04-21 12:50:14 -0400

    Code for getData

--- src/components/MasterList.js
+++ src/components/MasterList.js
@@ -1,65 +1,130 @@
 import React from "react";
+import "../style/MasterList.css";
 
 export default class MasterList extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         //Set Default Data
         this.state = {
-            masterData:[],
-            test: "TESTING!"
+            masterData: {},
+            title: "Master Item List",
+            showTitle: true
         }
 
+        this.getItemData();
+    }//constructor
 
+    getItemData() {
+
+        //Get List of All Items
         var url = "http://www.dnd5eapi.co/api/equipment";
         var parm = {
-          method: "GET",
+            method: "GET",
         };
 
         fetch(url, parm)
-        .then((response) => {
-          return response.json();
-        })
-        .then((json) => {
-          var masterData = json.results.map((data) =>{
-            return {
-              id: data.index,
-              display: data.name,
-              url: data.url
-            };
-          });
-          this.setState({masterData: masterData});
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                var masterData = {};
+                for (var i = 0; i < json.results.length; i++) {
+                    masterData[json.results[i].index] = {
+                        index: json.results[i].index,
+                        name: json.results[i].name,
+                        url: json.results[i].url,
+                        cost: {
+                            quantity: 0,
+                            unit: ""
+                        }
+                    }
+                }
 
-          console.log("Master Data");
-          console.log(masterData);
+                this.setState({ masterData: masterData });
+
+
+                //Get Cost of All Items
+                var fetches = [];
+                for (var key in masterData) {
+                    
+                    var url = "http://www.dnd5eapi.co/api/equipment/" + key;
+        
+                    fetches.push(
+                        fetch(url, parm)
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((json) => {
+                            
+                            masterData[json.index].cost.quantity = json.cost.quantity;
+                            masterData[json.index].cost.unit = json.cost.unit;
+
+                
+                        })
+                    );
+                }
+        
+                Promise.all(fetches).then(() =>{
+                    console.log("DONE");
+                    console.log(masterData);
+                });
+            });
+    }//getItemData
+
+
+
+    renderRows() {
+        var data = this.state.masterData;
+        var rows = [];
+
+        for (var key in data) {
+            rows.push({
+                "index": data[key].index,
+                "name": data[key].name,
+                "url": data[key].url,
+            });
+        }
+
+        return rows.map((item, i) => {
+            return (
+                <tr key={item.index}>
+                    <td><input type="checkbox"></input></td>
+                    <td>{item.name}</td>
+                    <td>100</td>
+                </tr>
+            );
         });
     }
 
     render() {
+        const renderTitle = () =>{
+            if(this.state.showTitle){
+                return <h1>{this.state.title}</h1>
+            }
+        }
+
         return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Cost</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Item 1</td>
-                            <td>Cost 1</td>
-                        </tr>
-                        <tr>
-                            <td>Item 2</td>
-                            <td>Cost 2</td>
-                        </tr>
-                        <tr>
-                            <td>Item 3</td>
-                            <td>Cost 3</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="masterlist">
+                {renderTitle()}
+                <div className="masterlist-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Item</th>
+                                <th>Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderRows()}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="masterlist-buttons">
+                    <button>Add</button>
+                </div>
+
             </div>
         );
     };
